47672c02e12ca817a19ce798162353d1e62c632a
Mats Groenlid <groenlid@gmail.com>
2012-08-09 21:59:37 +0200

    added support of other working directory

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-require_once './vendor/restler/restler/restler.php';
-require_once './controllers/authenticate.php';
-require_once './controllers/anime.php';
-require_once './controllers/episodes.php';
-require_once './controllers/users.php';
-require_once './controllers/search.php';
-require_once './rb.php';
-require_once './config/db.php';
+require_once getcwd().'/vendors/restler/restler/restler.php';
+require_once getcwd().'/controllers/authenticate.php';
+require_once getcwd().'/controllers/anime.php';
+require_once getcwd().'/controllers/episodes.php';
+require_once getcwd().'/controllers/users.php';
+require_once getcwd().'/controllers/search.php';
+require_once getcwd().'/rb.php';
+require_once getcwd().'/config/db.php';
 
 header('Access-Control-Allow-Origin: *');
 
